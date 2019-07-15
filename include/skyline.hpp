687ca9e212ff03c66103454623d442334ff80124
Jason W. DeGraw <jason.degraw@gmail.com>
2019-07-15 11:00:17 -0600

    Move to single storage array

--- include/skyline.hpp
+++ include/skyline.hpp
@@ -65,12 +65,11 @@ public:
       m_im[k] = k - m_ih[k];
     }
 
-    // Copy into the au/d array
-    m_ad.resize(n);
-    m_au.resize(sum);
+    // Copy into the am array
+    m_am.resize(n + sum);
     I count = 0;
     for (I i = 0; i < n; i++) {
-      m_ad[i] = M[i][i];
+      m_am[i] = M[i][i];
       I j;
       for (j = 0; j < i; j++) {
         if (M[i][j] != 0.0) {
@@ -78,7 +77,7 @@ public:
         }
       }
       for (; j < i; j++) {
-        m_au[count] = M[i][j];
+        m_am[n + count] = M[i][j];
         ++count;
       }
     }
@@ -102,15 +101,11 @@ public:
       m_im[k] = k - m_ih[k];
     }
 
-    // Size the au/d array
-    m_ad.resize(n);
-    for (I i = 0; i < n; i++) {
-      m_ad[i] = 0.0;
-    }
-
-    m_au.resize(sum);
-    for (I i = 0; i < sum; i++) {
-      m_au[i] = 0.0;
+    // Size the am array
+    I total{ n + sum };
+    m_am.resize(total);
+    for (I i = 0; i < total; i++) {
+      m_am[i] = 0.0;
     }
 
     m_v.resize(n);
@@ -119,8 +114,7 @@ public:
 
   void fill(R v = 0.0)
   {
-    std::fill(m_ad.begin(), m_ad.end(), v);
-    std::fill(m_au.begin(), m_au.end(), v);
+    std::fill(m_am.begin(), m_am.end(), v);
   }
 
   V<I> offsets() const
@@ -140,33 +134,35 @@ public:
 
   V<R> diagonal() const
   {
-    return m_ad;
+    return V<R>(m_am.begin(), m_am.begin() + m_n);
   }
 
   V<R> upper() const
   {
-    return m_au;
+    return V<R>(m_am.begin()+m_n, m_am.end());
   }
 
   V<R> lower() const
   {
-    return m_au;
+    return V<R>(m_am.begin() + m_n, m_am.end());
   }
 
   R &operator()(I i)
   {
-    return m_au[i];
+    return m_am[i];
   }
 
   R &diagonal(I i)
   {
-    return m_ad[i];
+    return m_am[i]; // This ends up being the same as operator()
   }
 
   std::optional<I> index(I i, I j) const
   {
     if (m_im[j] <= i) {
-      return m_ik[j] + i - m_im[j];
+      return m_n + m_ik[j] + i - m_im[j];
+    } else if (m_im[j] == i) {
+      return i;
     }
     return {};
   }
@@ -176,8 +172,8 @@ public:
     // j = 0, nothing much to do
     for (I k = 1; k < m_n; ++k) {
       if (m_im[k] == 0) {
-        I ij = m_ik[k];
-        m_au[ij] /= m_ad[0];
+        I ij = m_n + m_ik[k];
+        m_am[ij] /= m_am[0];
       }
     }
     // Now for the rest
@@ -187,24 +183,24 @@ public:
         m_v[i] = 0.0;
       }
       for (I i = m_im[j]; i < j; ++i) {
-        m_v[i] = m_au[m_ik[j] + i - m_im[j]] * m_ad[i]; // OK, i >= m_im[j]
+        m_v[i] = m_am[m_n + m_ik[j] + i - m_im[j]] * m_am[i]; // OK, i >= m_im[j]
       }
       // Compute the diagonal term
       R value = 0.0;
       for (I i = m_im[j]; i < j; ++i) {
-        value += m_au[m_ik[j] + i - m_im[j]] * m_v[i];  // OK, i >= m_im[j]
+        value += m_am[m_n + m_ik[j] + i - m_im[j]] * m_v[i];  // OK, i >= m_im[j]
       }
-      m_ad[j] -= value;
+      m_am[j] -= value;
       // Compute the rest of the row
       for (I k = j + 1; k < m_n; ++k) {
         if (m_im[k] <= j) {
           value = 0.0;
           for (I i = m_im[k]; i < j; ++i) {
-            I ij = m_ik[k] + i - m_im[k]; // OK, i >= m_im[k]
-            value += m_au[ij] * m_v[i];
+            I ij = m_n + m_ik[k] + i - m_im[k]; // OK, i >= m_im[k]
+            value += m_am[ij] * m_v[i];
           }
-          I ij = m_ik[k] + j - m_im[k]; // OK, j >= m_im[k]
-          m_au[ij] = (m_au[ij] - value) / m_ad[j];
+          I ij = m_n + m_ik[k] + j - m_im[k]; // OK, j >= m_im[k]
+          m_am[ij] = (m_am[ij] - value) / m_am[j];
         }
       }
     }
@@ -216,8 +212,8 @@ public:
     for (I i = 1; i < m_n; ++i) {
       R value = 0.0;
       for (I k = m_im[i]; k < i; ++k) {
-        I ij = m_ik[i] + k - m_im[i];
-        value += m_au[ij] * b[k];
+        I ij = m_n + m_ik[i] + k - m_im[i];
+        value += m_am[ij] * b[k];
       }
       b[i] -= value;
     }
@@ -227,13 +223,13 @@ public:
   {
     // Account for the diagonal first (invert Dy=z)
     for (I j = 0; j < m_n; ++j) {
-      z[j] /= m_ad[j];
+      z[j] /= m_am[j];
     }
     // Solve Ux=y
     for (I j = m_n - 1; j > 0; --j) {
       for (I k = m_im[j]; k < j; ++k) {
-        I ij = m_ik[j] + k - m_im[j];
-        z[k] -= z[j] * m_au[ij];
+        I ij = m_n + m_ik[j] + k - m_im[j];
+        z[k] -= z[j] * m_am[ij];
       }
     }
   }
@@ -261,8 +257,9 @@ private:
   V<I> m_ik; // Index offsets to top of skylines
   V<I> m_ih; // Height of each skyline (not used, should probably be removed)
   V<I> m_im; // Minimum row, or top of skyline
-  V<R> m_au; // Upper triangular part of matrix
-  V<R> m_ad; // Diagonal of matrix
+  V<R> m_am; // The entire matrix in one vector, first the diagonal, then the rest
+  //V<R> m_au; // Upper triangular part of matrix
+  //V<R> m_ad; // Diagonal of matrix
   V<R> m_v;  // Temporary used in solution
 };
 
